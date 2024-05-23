db9a21a2c214f11665ff22b71f9f9d15d93af1eb
vBarbaros <victor.barbarosh@gmail.com>
2024-05-23 19:06:55 -0400

    Add logging for future augment metrics

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create a file to store PIDs
-pid_file="training_pids.txt"
-> "$pid_file"  # Clear the file if it already exists
+#pid_file="training_pids.txt"
+#> "$pid_file"  # Clear the file if it already exists
 
 # Loop from 2 to 10, incrementing by 2
 #python get_augment_stats.py task=pendulum_swingup agent=drqv2 &
@@ -14,7 +14,7 @@ for i in $(seq 0.5 0.5 15); do
     # Run the Python script with the current agent value in the background
     python get_augment_stats.py task=pendulum_swingup agent=drqv2_aug augment_type=contrast augment_param=$i
     # Capture the PID of the last background process and store it
-    echo $! >> "$pid_file"
+#    echo $! >> "$pid_file"
 done
 
 # Display the PIDs stored
