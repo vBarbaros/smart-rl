27be5dd987b2b95c3761761e61b744a820ae441d
vBarbaros <victor.barbarosh@gmail.com>
2024-05-23 19:04:53 -0400

    Add logging for future augment metrics

--- train.py
+++ train.py
@@ -10,8 +10,8 @@ warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 import os
 os.environ['MKL_SERVICE_FORCE_INTEL'] = '1'
-os.environ['MUJOCO_GL'] = 'glfw'
-# os.environ['MUJOCO_GL'] = 'osmesa'
+# os.environ['MUJOCO_GL'] = 'glfw'
+os.environ['MUJOCO_GL'] = 'osmesa'
 
 from pathlib import Path
 
@@ -27,9 +27,10 @@ from utils.replay_buffer import ReplayBufferStorage, make_replay_loader
 from utils.video import TrainVideoRecorder, VideoRecorder
 
 
-# torch.backends.cudnn.benchmark = True
-#torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
+#torch.backends.cudnn.benchmark = False
+
 
 
 def make_agent(obs_spec, action_spec, cfg):
