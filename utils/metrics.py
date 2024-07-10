631942dea367b2e3bbe814564247bcd4665d4fdb
vBarbaros <victor.barbarosh@gmail.com>
2024-07-10 14:38:26 -0400

    Add sharpness minimal augmenter

--- utils/metrics.py
+++ utils/metrics.py
@@ -26,7 +26,21 @@ class StatsMetrics:
         return self.metrics
 
     def compute_all_metrics(self, img_tensor_original, img_tensor_augmented):
-        # self.ssim_dist(img_tensor_original, img_tensor_augmented)
+        def normalize_tensor(tensor):
+            tensor = tensor.clone()
+            tensor = tensor - tensor.min()
+            tensor = tensor / tensor.max() * 255.0
+            return tensor
+
+        def check_and_normalize(tensor):
+            if tensor.max() > 255.0 or tensor.min() < 0.0:
+                tensor = normalize_tensor(tensor)
+            return tensor
+
+        # Check and normalize if necessary
+        img_tensor_original = check_and_normalize(img_tensor_original)
+        img_tensor_augmented = check_and_normalize(img_tensor_augmented)
+
         self.hamming(img_tensor_original, img_tensor_augmented)
         self.bhattacharyya(img_tensor_original, img_tensor_augmented)
         self.kl_div(img_tensor_original, img_tensor_augmented)
